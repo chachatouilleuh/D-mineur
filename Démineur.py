7fae14e724590bb98fb4410a9cc80c805190b3f8
GAME1.1 <GAME1.1@etpa.local>
2020-11-24 18:02:16 +0100

    Update Démineur.py

--- Démineur.py
+++ Démineur.py
@@ -1,37 +1,98 @@
-#listgénérale=[ligne1,ligne2,ligne3]
-#ligne1=[]
-#ligne2=[]
-#ligne3=[]
+#Charly Despeyroux
 
-tableau=[]
-largeur="x"
-hauteur="x"
-def init(largeur, hauteur):
-    def tableau():
-        difficulté=input("Choisissez un niveau facile=1 , moyen=2, difficile=3 :") 
-        if difficulté==1:
-            largeur=5
-            hauteur=5
-        elif difficulté==2:
-            largeur=15
-            hauteur=15
-        else:
-            largeur=50
-            hauteur=50
-    return init(tableau,largeur,hauteur)
+import random
 
-def init(tableau,largeur,hauteur):
-    for i in hauteur:
+largeur=0
+hauteur=0
+
+def demandeDifficulte():
+    global largeur
+    global hauteur
+    difficulté=int(input("Choisissez un niveau facile=1 , moyen=2, difficile=3 :")) 
+    if difficulté==1:
+        largeur=5
+        hauteur=5
+
+    elif difficulté==2:
+        largeur=15
+        hauteur=15
+    else:
+        largeur=50
+        hauteur=50
+
+def initTableau(largeur,hauteur):
+    tableau=[]
+    for i in range(hauteur):
         tableau.append([])
-        for j in largeur:
+        for j in range(largeur):
             tableau[i].append("x")
+    return tableau
+
+def initMines(largeur,hauteur):
+    tableau=[]
+    for i in range(hauteur):
+        tableau.append([])
+        for j in range(largeur):
+            tableau[i].append(0)
+    return tableau
+
+
+def afficher(tableau):
+    print("  0 1 2 3 4")
+    for i in range(0,len(tableau)):
+        print(i, end=" ")
+        for j in range(0,len(tableau[0])):
+               print (tableau[i][j], end=" ")      
+        print("")
+
+def placerbombe(tableau, nbBombe):
+    i=0
+    while i<nbBombe:
+        ligne = random.randint(0,len(tableau)-1)
+        colonne = random.randint(0,len(tableau[0])-1)
+        if (tableau[ligne][colonne]!="b"):
+            tableau[ligne][colonne]="b"
+            i += 1
+
+
+def demineur():
+    bombe=0
+    tour=0
+    i=0
+    global largeur
+    global hauteur 
+    demandeDifficulte()
     
+    ligne = largeur
+    colonne = hauteur
+    nbBombe= (largeur*hauteur)//4
 
-#initialiser mines
-#maj demineur (0,1,2)
-#si 0 >>> ouvre la case voisine
-#si mine >>> perdu
-#si nombre de cases restantes = nombre de mines >>> gagné
-#le joueur rentre une info
-#input():
 
+    plateau = initTableau(ligne,colonne)
+    plateauMines = initMines(ligne,colonne)
+
+    afficher(plateau)
+    afficher(plateauMines)
+    #je place les bombes
+    placerbombe(plateauMines, nbBombe)
+    afficher(plateauMines)
+
+
+    choixJoueur=0
+    #je demande au joueur d'entrer une coordonnée
+    while (tour<len(plateau)*len(plateau[0])-bombe and choixJoueur != "b"):
+        choixJoueur=0
+        print('choisir une ligne :', end=" ")
+        x = int(input())
+        print('choisir une colonne :', end=" ")
+        y = int(input())
+        choixJoueur=plateauMines[x][y]
+        if choixJoueur == "b":
+            print ("T un raT")
+        else: 
+            print ("Yay, continue !")
+            #plateau.replace(choixJoueur)
+            #afficher(plateau)
+        tour += 1 
+
+demineur()
